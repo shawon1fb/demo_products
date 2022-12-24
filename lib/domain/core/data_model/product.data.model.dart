5898a75ea4f2da5cb0c954a0fd28090bb9089b16
shawon1fb <shawon0187@gmail.com>
2022-12-25 01:43:17 +0600

    product data model

---
+++ lib/domain/core/data_model/product.data.model.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+class ProductDataModel {
+  int id;
+  String title;
+
+  double price;
+  String description;
+  String category;
+  String image;
+
+  ProductDataModel({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+  });
+
+  @override
+  bool operator ==(Object other) {
+    if (other is ProductDataModel) {
+      return hashCode == other.hashCode;
+    }
+    return false;
+  }
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'description': description,
+        'category': category,
+        'image': image,
+        'price': price,
+      };
+
+  @override
+  String toString() {
+    return const JsonEncoder.withIndent(' ').convert(toJson());
+  }
+
+  @override
+  int get hashCode => id.hashCode + title.hashCode;
+}
