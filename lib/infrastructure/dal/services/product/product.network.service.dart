4d1e73b9a5a10db410c45dceb596f7301da0a84c
shawon1fb <shawon0187@gmail.com>
2022-12-25 01:46:36 +0600

    product service

--- lib/infrastructure/dal/services/product/product.network.service.dart
+++ lib/infrastructure/dal/services/product/product.network.service.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import '../../../../domain/core/abstractions/http_connect.interface.dart';
+import '../../../../domain/core/exceptions/default.exception.dart';
+import '../../../../domain/core/exceptions/forbidden.exception.dart';
+import '../../../../domain/core/exceptions/unauthorized.exception.dart';
+import '../../../../domain/core/exceptions/unprocessable.entity.exception.dart';
 import '../usecase/products.service.interface.dart';
 import 'request/all_products.response.dart';
 
@@ -7,11 +13,57 @@ class ProductNetworkService extends IProductService {
 
   ProductNetworkService({required IHttpConnect connect}) : _connect = connect;
 
-  String get _prefix => 'auth';
+  String get _prefix => 'products';
 
   @override
-  Future<AllProductsResponse> getAllProducts() {
-    // TODO: implement getAllProducts
-    throw UnimplementedError();
+  Future<AllProductsResponse> getAllProducts() async {
+    try {
+      final response = await _connect.get<AllProductsResponse>(
+        _prefix,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          // 'user-Agent': 'PostmanRuntime/7.29.2',
+        },
+        decoder: (value) {
+          return AllProductsResponse.fromJson(
+            value is String
+                ? json.decode(value)
+                : value as Map<String, dynamic>,
+          );
+        },
+      );
+      if (response.success) {
+        return response.payload!;
+      } else {
+        switch (response.statusCode) {
+          case 401:
+            throw UnAuthorizedException(
+              message:
+                  response.payload?.message ?? 'invalid username or password',
+            );
+          case 403:
+            throw ForbiddenException(
+              message:
+                  response.payload?.message ?? 'invalid username or password',
+            );
+          case 422:
+            throw UbProcessAbleEntityException(
+                message: response.payload?.message ??
+                    'UbProcessAbleEntityException');
+          case 500:
+            throw DefaultException(
+              message: response.payload?.message ?? 'Server error',
+            );
+          default:
+            throw DefaultException(
+              message: response.payload?.message ??
+                  'Error loading data, check your internet!',
+            );
+        }
+      }
+    } catch (e) {
+      rethrow;
+    }
   }
 }
