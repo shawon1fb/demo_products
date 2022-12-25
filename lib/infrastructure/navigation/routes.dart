d044ed096b0ad9afd0b999cd22745123b05ad3bb
shawon1fb <shawon0187@gmail.com>
2022-12-25 12:29:21 +0600

    initial route

--- lib/infrastructure/navigation/routes.dart
+++ lib/infrastructure/navigation/routes.dart
@@ -1,6 +1,17 @@
+import 'bindings/domains/app.data.repository.bindings.dart';
+
 class Routes {
   static Future<String> get initialRoute async {
-    // TODO: implement method
+    var repo = AppDataRepositoryBinding().repository;
+
+    try {
+      String token = await repo.getJwtToken();
+      if (token.isEmpty) {
+        return LOGIN;
+      } else {
+        return HOME;
+      }
+    } catch (_) {}
     return LOGIN;
   }
 
